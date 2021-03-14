055a9c22184a3203ca4b7caa0a60b41412fb8fd8
Alucard <smpark@ezstorage.co.kr>
2021-03-14 20:17:40 +0900

    get user sign via dialog

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Button, TextField, Container, Box } from "@material-ui/core";
 import { useState } from "react";
+import Document from "./Document";
 
 function App() {
   const [ID, setID] = useState(""); // ID 의 상태(데이터)를 관리
   const [password, setPassword] = useState(""); // PW 의 상태(데이터)를 관리
 
   return (
-    <Container maxWidth="sm">
-      <Box display="flex" flexDirection="column" pb={2} bgcolor="primary.main">
+    <Container maxWidth="lg">
+      <Box display="flex" flexDirection="column" pb={2}>
         <TextField
           onChange={(e) => {
             setID(e.target.value);
@@ -35,6 +36,9 @@ function App() {
         >
           로그인
         </Button>
+      </Box>
+      <Box>
+        <Document />
       </Box>
     </Container>
   );
---
+++ src/Document.js
@@ -0,0 +1,206 @@
+import ReactSignatureCanvas from "react-signature-canvas";
+import React, { useRef, useState } from "react";
+import { makeStyles, styled } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import MuiTableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Typography,
+} from "@material-ui/core";
+
+const useStyles = makeStyles({
+  signPad: {
+    backgroundColor: "transparent",
+    width: "100%",
+    height: "100%",
+  },
+  dialogContent: {
+    height: 100,
+  },
+});
+
+const TableCell = styled(MuiTableCell)({
+  border: "1px solid black",
+});
+
+const SignTypo = styled(Typography)({
+  whiteSpace: "pre",
+  fontSize: 16,
+});
+
+const SignnedImage = styled("img")({
+  width: "100px",
+  height: "25px",
+  position: "absolute",
+  right: -100,
+  bottom: -20,
+});
+
+const Document = () => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const padRef = useRef();
+  const [trimmedDataURL, setTrimmedDataURL] = useState("");
+  const trim = () => {
+    setTrimmedDataURL(padRef.current.getTrimmedCanvas().toDataURL("image/png"));
+  };
+  const handleOpen = () => setOpen(true);
+
+  console.log(trimmedDataURL);
+  return (
+    <>
+      <TableContainer>
+        <Table aria-label="spanning table" padding="checkbox" size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>항목</TableCell>
+              <TableCell>세부항목</TableCell>
+              <TableCell>해당사항</TableCell>
+              <TableCell>비고</TableCell>
+              <TableCell>항목</TableCell>
+              <TableCell>세부항목</TableCell>
+              <TableCell>해당사항</TableCell>
+              <TableCell>비고</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell variant="head" rowSpan={2}>
+                검사
+              </TableCell>
+              <TableCell>초음파</TableCell>
+              <TableCell padding="none" align="center">
+                <Checkbox size="small" />
+              </TableCell>
+              <TableCell></TableCell>
+              <TableCell rowSpan={8}>기타</TableCell>
+              <TableCell>조직재생주사 (DNA)</TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>적외선 체열검사</TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+              <TableCell>유착박리제</TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell variant="head" rowSpan={5}>
+                치료
+              </TableCell>
+              <TableCell>체외충격파치료</TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+              <TableCell>ATP</TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Cryotherapy</TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+              <TableCell>vit D</TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>도수치료</TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+              <TableCell>혈관 영양주사</TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Painscrambler</TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+              <TableCell>대상포진 예방접종</TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>증식치료</TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+              <TableCell>독감 예방접종</TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell variant="head">기타</TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+              <TableCell>토마스칼라(목보호대)</TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Box display="flex" flexDirection="column" alignItems="flex-end" p={3}>
+        <SignTypo style={{ whiteSpace: "pre" }} align="right">
+          {"2021년          월          일"}
+        </SignTypo>
+        <SignTypo style={{ textDecoration: "underline" }} gutterBottom>
+          {"동의인:            인"}
+        </SignTypo>
+        <Box position="relative">
+          <SignTypo
+            onClick={handleOpen}
+            style={{ textDecoration: "underline", cursor: "pointer" }}
+          >
+            {"환자와의 관계:         인"}
+          </SignTypo>
+          {trimmedDataURL ? <SignnedImage src={trimmedDataURL} /> : null}
+        </Box>
+      </Box>
+      <Dialog
+        onClose={() => setOpen(false)}
+        open={open}
+        maxWidth="xs"
+        fullWidth
+      >
+        <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
+        <DialogContent className={classes.dialogContent}>
+          <ReactSignatureCanvas
+            penColor="black"
+            dotSize={5}
+            minWidth={1}
+            maxWidth={3}
+            canvasProps={{
+              className: classes.signPad,
+            }}
+            ref={padRef}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              trim();
+              setOpen(false);
+            }}
+          >
+            Submit
+          </Button>
+          <Button>Cancel</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default Document;
